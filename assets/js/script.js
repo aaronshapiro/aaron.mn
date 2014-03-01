05d9b8ea6a5d62e04560a49da3d7b03ac78d6806
Aaron Shapiro <aaronshapiro@2268.local>
2014-03-01 00:30:03 -0500

    Info panel animation

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.info-link').click(function() {
+        $('.info-wrapper').addClass('info-wrapper-open');
+    });
+    $('.info-wrapper').click(function() {
+        if ($(this).hasClass('info-wrapper-open')) {
+            $(this)
+                .addClass('info-wrapper-closing')
+                .on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                    $(this)
+                        .removeClass('info-wrapper-open info-wrapper-closing')
+                        .off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+                });
+        }
+    });
+});
+
