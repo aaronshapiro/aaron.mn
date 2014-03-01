e5fb677105f14c26db075ebf9c7faafd1981fe85
Aaron Shapiro <aaronshapiro@2268.local>
2014-03-01 12:35:26 -0500

    Switched hidden class to use display: none because of some Safari weirdness

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,35 +3,31 @@ $(document).ready(function() {
     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
     $('.info-link').click(function() {
-        if ($('.info-wrapper').hasClass('hidden')) {
-            $('.info-wrapper')
-                .removeClass('hidden')
-                .addClass('bounce-in-right')
-                .on(animationEnd, function() {
-                    $('.info-wrapper')
-                        .removeClass('bounce-in-right')
-                        .off(animationEnd);
-                    $('.info-mask')
-                        .removeClass('hidden');
-                });
-            $('.info-wrapper .info').addClass('fade-in').removeClass('fade-out');
-        }
+        $('.info-wrapper')
+            .removeClass('hidden')
+            .addClass('bounce-in')
+            .on(animationEnd, function() {
+                $('.info-wrapper')
+                    .removeClass('bounce-in')
+                    .off(animationEnd);
+                
+            });
+        $('.info-mask').removeClass('hidden');
+        $('.info-wrapper .info').addClass('fade-in').removeClass('fade-out');
         return false;
     });
 
     $('.info-close').click(function() {
-        if (!$('.info-wrapper').hasClass('hidden')) {
-            $('.info-wrapper')
-                .addClass('bounce-out-right')
-                .on(animationEnd, function() {
-                    $(this)
-                        .addClass('hidden')
-                        .removeClass('bounce-out-right')
-                        .off(animationEnd);
-                    $('.info-mask').addClass('hidden');
-                });
+        $('.info-wrapper')
+            .addClass('bounce-out')
+            .on(animationEnd, function() {
+                $(this)
+                    .addClass('hidden')
+                    .removeClass('bounce-out')
+                    .off(animationEnd);
+                $('.info-mask').addClass('hidden');
+            });
             $('.info-wrapper .info').addClass('fade-out').removeClass('fade-in');
-        }
         return false;
     });
 
