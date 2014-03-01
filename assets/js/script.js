b1cb0286eccfe5673e767fbabdcf9613f9f96954
Aaron Shapiro <aaronshapiro@2268.local>
2014-03-01 03:33:41 -0500

    Animation refinements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
+
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
     $('.info-link').click(function() {
         $('.info-wrapper').addClass('info-wrapper-open');
+        $('.info-wrapper .info').addClass('fadeIn').removeClass('fadeOut');
         return false;
     });
     $('.info-wrapper').click(function() {
         if ($(this).hasClass('info-wrapper-open')) {
             $(this)
                 .addClass('info-wrapper-closing')
-                .on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                .on(animationEnd, function() {
                     $(this)
                         .removeClass('info-wrapper-open info-wrapper-closing')
-                        .off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+                        .off(animationEnd);
                 });
+            $('.info-wrapper .info').addClass('fadeOut').removeClass('fadeIn');
         }
         return false;
     });
