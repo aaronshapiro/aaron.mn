0b05b66a52474e01151c06d98edb101c000e497a
Aaron Shapiro <aaronshapiro@2268.local>
2014-03-01 11:06:45 -0500

    Fixed scrolling on x-axis bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,29 @@ $(document).ready(function() {
     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
     $('.info-link').click(function() {
-        $('.info-wrapper').addClass('info-wrapper-open');
-        $('.info-wrapper .info').addClass('fadeIn').removeClass('fadeOut');
+        if (!$('.info-wrapper').hasClass('info-wrapper-open')) {
+            $('.info-wrapper')
+                .addClass('bounce-in-right info-wrapper-open')
+                .on(animationEnd, function() {
+                    $('.info-wrapper')
+                        .removeClass('bounce-in-right')
+                        .off(animationEnd);
+                });
+            $('.info-wrapper .info').addClass('fade-in').removeClass('fade-out');
+        }
         return false;
     });
+
     $('.info-wrapper').click(function() {
         if ($(this).hasClass('info-wrapper-open')) {
             $(this)
-                .addClass('info-wrapper-closing')
+                .addClass('bounce-out-right')
                 .on(animationEnd, function() {
                     $(this)
-                        .removeClass('info-wrapper-open info-wrapper-closing')
+                        .removeClass('info-wrapper-open bounce-out-right')
                         .off(animationEnd);
                 });
-            $('.info-wrapper .info').addClass('fadeOut').removeClass('fadeIn');
+            $('.info-wrapper .info').addClass('fade-out').removeClass('fade-in');
         }
         return false;
     });
